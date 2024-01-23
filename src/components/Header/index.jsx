70e4ce70640b9ec0f85bc5ea6335836ad0cbb8cd
afrguilherme <augustogafr@gmail.com>
2024-01-23 11:05:07 -0300

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import Logo from "../../assets/logo.png"
+
+function Header() {
+  return (
+    <div>
+      <img src={Logo} alt="header-logo" style={{ width: "100px" }} />
+    </div>
+  )
+}
+
+export default Header
